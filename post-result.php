467d53acaba90fad4403d9096054570cc70556b0
Franck D K <98106173+Franck-DK@users.noreply.github.com>
2024-11-23 22:51:31 -0600

    Update post-result.php

--- post-result.php
+++ post-result.php
@@ -2,22 +2,18 @@
 $pageTitle= "Post result";
 include "view-header.php";
 ?>
-    <h1>Post Result</h1>
-
-  <?php
-
-if (isset($_POST['my_name'])) 
+<h1>Post Result</h1>
+<?php
+if (isset($_POST['my-name'])) 
 {
   ?>
-  <p>The value sent is: </p>
+  <p>The value sent is:</p>
   <?php
-  echo $_POST['my_name'];
-} else
-{
+  echo $_POST['my-name'];
+} else {
   ?>
   <p>Nothing posted to the page.</p>
   <?php
 }
-
 include "view-footer.php";
 ?>
