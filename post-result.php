3c5dd74f229f237fb439d6d29fdb46b51f65d99a
Franck D K <98106173+Franck-DK@users.noreply.github.com>
2024-12-10 22:20:29 -0600

    Update post-result.php

--- post-result.php
+++ post-result.php
@@ -4,16 +4,15 @@ include "view-header.php";
 ?>
 <h1>Post Result</h1>
 <?php
-if (isset($_POST['my-name'])) 
-{
-  ?>
+if (isset($_POST['my-name'])) {
+?>
   <p>The value sent is:</p>
-  <?php
+<?php
   echo $_POST['my-name'];
 } else {
-  ?>
+?>
   <p>Nothing posted to the page.</p>
-  <?php
+<?php
 }
 include "view-footer.php";
 ?>
